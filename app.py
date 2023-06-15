25726ee9cc29a251e6cfee0b09a41e5420663898
bln121 <77780135+bln121@users.noreply.github.com>
2023-06-15 10:11:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def login_page():
     st.header("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    
+    st.button("Login")
 login_page() 
  
     
